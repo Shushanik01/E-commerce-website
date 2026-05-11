5d179bf27d91a1f3d60ae70f3b89a695a312c59a
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-11 23:58:37 +0400

    productSlice created

--- vite-project/src/slices/productSlice.ts
+++ vite-project/src/slices/productSlice.ts
@@ -1,8 +1,31 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-// const productSlice = createSlice({
-//     name: 'productSlice'
-// })
+const initialState = {
+    data: [],
+    loading: false,
+    error: null
+};
+
+const productSlice = createSlice({
+    name: 'productSlice',
+    initialState,
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchProducts.pending, (state)=>{
+            state.loading = true;
+            state.error = null
+        })
+        .addCase(fetchProducts.fulfilled, (state, action)=>{
+            state.loading = false;
+            state.data = action.payload
+        })
+        .addCase(fetchProducts.rejected, (state, action)=>{
+            state.error = action.payload || 'Something went wrong!';
+            state.loading = false
+        })
+    }
+})
 
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
@@ -13,6 +36,10 @@ export const fetchProducts = createAsyncThunk(
         if(!response.ok){
             throw new Error('Unsuccesfull product fetch')
         }
+        const data = await response.json();
+        return data
+    } catch(error){
+        return thunkAPI.rejectWithValue(error.message)
     }
   }
 );
