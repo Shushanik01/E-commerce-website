722556c98dc15116bfd84548a078297e5924d577
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-11 23:49:50 +0400

    productSlice created

--- vite-project/src/slices/productSlice.ts
+++ vite-project/src/slices/productSlice.ts
@@ -1,10 +1,18 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+// const productSlice = createSlice({
+//     name: 'productSlice'
+// })
+
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
-  async () => {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const data = await response.json();
-    return data;
+  async(_, thunkAPI) =>{
+    try{
+        const response = await fetch('https://fakestoreapi.com/products');
+
+        if(!response.ok){
+            throw new Error('Unsuccesfull product fetch')
+        }
+    }
   }
 );
