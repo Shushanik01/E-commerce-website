be4d56c1e3fc4d19a378061a34368bf529d021ce
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-12 22:20:39 +0400

    configure gh-pages deployment

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
